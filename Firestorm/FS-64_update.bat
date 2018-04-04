42ca72562da1851f0d27fc9f4e5df8096dc344be
Vincent Sylvester <vincentsylvester@zetamex.com>
2018-04-04 07:35:32 +0200

    Changing structure, cleanup, adding list of available grids, added Singularity

--- Firestorm/FS-64_update.bat
+++ Firestorm/FS-64_update.bat
@@ -1,7 +1,7 @@
 @echo off
 del grids.remote.xml >nul 2>&1
 echo Fetching latest version...
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Tampa/OpenGridList/master/grids.remote.xml', 'grids.remote.xml')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Tampa/OpenGridList/master/Firestorm/grids.remote.xml', 'grids.remote.xml')"
 echo "Copying to user settings..."
 echo F|xcopy grids.remote.xml %APPDATA%\Firestorm_x64\user_settings\grids.remote.xml /s /y /Q >nul 2>&1
 echo Cleaning up...
