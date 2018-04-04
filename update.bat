0c3beb9256a89ffeb63b958cd8730b34fb292515
Vincent Sylvester <vincentsylvester@zetamex.com>
2018-04-04 02:10:26 +0200

    Automatic update.bat initial commit

---
+++ update.bat
@@ -0,0 +1,10 @@
+@echo off
+del grids.remote.xml >nul 2>&1
+echo Fetching latest version...
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Tampa/OpenGridList/master/grids.remote.xml', 'grids.remote.xml')"
+echo "Copying to user settings..."
+echo F|xcopy grids.remote.xml %APPDATA%\Firestorm_x64\user_settings\grids.remote.xml /s /y /Q >nul 2>&1
+echo Cleaning up...
+del grids.remote.xml >nul 2>&1
+echo Done, you may close this window now!
+@pause on
